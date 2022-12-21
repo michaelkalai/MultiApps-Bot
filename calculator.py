43bf296b313c01555ebeafb803669ed853f71feb
Michael Kalai <113479090+michaelkalai@users.noreply.github.com>
2022-12-21 23:41:17 +0000

    Added exponents and roots to calculator

--- calculator.py
+++ calculator.py
@@ -11,11 +11,16 @@ class Calculator(discord.ui.View):
     self.selected = ""
     self.type = "deg"
     self.answer = "0"
-
+  
   def emb(self):
     dg_rd = "ᵈᵉᵍ" if self.type == "deg" else "ʳᵃᵈ"
     embed = discord.Embed(color = 0xFF5733)
-    embed.add_field(name = "Calculator", value = F"{dg_rd}\n{self.value1} {self.operation} {self.value2}")
+    if self.operation == "^":
+      embed.add_field(name = "Calculator", value=F"{dg_rd}\n{self.value1}^({self.value2})")
+    elif self.operation == "√":
+      embed.add_field(name = "Calculator", value=F"{dg_rd}\n{self.value1}√({self.value2})")
+    else:
+      embed.add_field(name = "Calculator", value = F"{dg_rd}\n{self.value1} {self.operation} {self.value2}")
     return embed
 
   def change_val(self):
@@ -25,6 +30,8 @@ class Calculator(discord.ui.View):
           self.value1 += self.selected
         else:
           self.value1 = self.selected
+      elif self.operation != "" and self.value2 == "":
+        self.value2 = "π"
       elif self.operation != "" and "π" not in self.value2 and self.value2[-1] != ".":
         self.value2 += self.selected
     elif self.selected.isdigit():
@@ -64,14 +71,68 @@ class Calculator(discord.ui.View):
         new_value = float(self.value1)
     return new_value
 
-  def cor_sign(self):
-    if float(self.answer) == 0 and "-" in self.answer:
-      self.answer = self.answer[1:]
-
   def call_error(self):
     self.value1 = "Error"
     self.value2 = ""
     self.operation = ""
+
+  @discord.ui.button(label = "d/r", row = 0, style = discord.ButtonStyle.red)
+  async def deg_rad(self, interaction: discord.Interaction, button: discord.ui.Button):
+    self.type = "deg" if self.type == "rad" else "rad"
+    embed = self.emb()
+    await interaction.response.edit_message(embed=embed)
+  
+  @discord.ui.button(label = "Sin", row = 1, style = discord.ButtonStyle.red)
+  async def sin(self, interaction: discord.Interaction, button: discord.ui.Button):
+    if self.operation == "" and self.value2 == "":
+      self.answer = round(math.sin(self.pi_con(self.value1)), 10)
+      if float(self.answer) == int(self.answer):
+        self.answer = int(self.answer)
+      self.answer = str(self.answer)
+      self.value1 = self.answer
+      self.value2 = ""
+      self.operation = ""
+    else:
+      self.call_error()
+    embed = self.emb()
+    await interaction.response.edit_message(embed=embed)
+
+  @discord.ui.button(label = "Cos", row = 2, style = discord.ButtonStyle.red)
+  async def cos(self, interaction: discord.Interaction, button: discord.ui.Button):
+    if self.operation == "" and self.value2 == "":
+      self.answer = round(math.cos(self.pi_con(self.value1)), 10)
+      if float(self.answer) == int(self.answer):
+        self.answer = int(self.answer)
+      self.answer = str(self.answer)
+      self.value1 = self.answer
+      self.value2 = ""
+      self.operation = ""
+    else:
+      self.call_error()
+    embed = self.emb()
+    await interaction.response.edit_message(embed=embed)
+
+  @discord.ui.button(label = "Tan", row = 3, style = discord.ButtonStyle.red)
+  async def tan(self, interaction: discord.Interaction, button: discord.ui.Button):
+    if self.operation == "" and self.value2 == "":
+      self.answer = round(math.tan(self.pi_con(self.value1)), 10)
+      if float(self.answer) == int(self.answer):
+        self.answer = int(self.answer)
+      self.answer = str(self.answer)
+      self.value1 = self.answer
+      self.value2 = ""
+      self.operation = ""
+    else:
+      self.call_error()
+    embed = self.emb()
+    await interaction.response.edit_message(embed=embed)
+
+  @discord.ui.button(label = "π", row = 4, style = discord.ButtonStyle.red)
+  async def pi(self, interaction: discord.Interaction, button: discord.ui.Button):
+    self.selected = "π"
+    self.change_val()
+    embed = self.emb()
+    await interaction.response.edit_message(embed=embed)
   
   @discord.ui.button(label = "C", row = 0, style = discord.ButtonStyle.blurple)
   async def clear(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -80,7 +141,7 @@ class Calculator(discord.ui.View):
     self.operation = ""
     embed = self.emb()
     await interaction.response.edit_message(embed=embed)
-
+  
   @discord.ui.button(label = "1", row = 1, style = discord.ButtonStyle.blurple)
   async def one(self, interaction: discord.Interaction, button: discord.ui.Button):
     self.selected = "1"
@@ -177,12 +238,12 @@ class Calculator(discord.ui.View):
     embed = self.emb()
     await interaction.response.edit_message(embed=embed)
 
-  @discord.ui.button(label = "dg/rd", row = 0, style = discord.ButtonStyle.red)
-  async def deg_rad(self, interaction: discord.Interaction, button: discord.ui.Button):
-    self.type = "deg" if self.type == "rad" else "rad"
+  @discord.ui.button(label = "^/√", row = 0, style = discord.ButtonStyle.blurple)
+  async def ex_root(self, interaction: discord.Interaction, button: discord.ui.Button):
+    self.operation = "^" if self.operation == "" or self.operation == "√" else "√"
     embed = self.emb()
     await interaction.response.edit_message(embed=embed)
-
+  
   @discord.ui.button(label = "+", row = 0, style = discord.ButtonStyle.green)
   async def plus(self, interaction: discord.Interaction, button: discord.ui.Button):
     self.selected = "+"
@@ -221,24 +282,32 @@ class Calculator(discord.ui.View):
   @discord.ui.button(label = "=", row = 4, style = discord.ButtonStyle.green)
   async def equal(self, interaction: discord.Interaction, button: discord.ui.Button):
     try:
-      if "π" in self.value1:
-        value1 = float(self.value1[:-1]) * math.pi
+      if self.operation != "":
+        if "π" in self.value1:
+          value1 = float(self.value1[:-1]) * math.pi
+        else:
+          value1 = float(self.value1)
+        if "π" in self.value2:
+          value2 = float(self.value2[:-1]) * math.pi
+        else:
+          value2 = float(self.value2)
+        if self.operation == "+":
+          self.answer = value1 + value2
+        elif self.operation == "-":
+          self.answer = value1 - value2
+        elif self.operation == "X":
+          self.answer = value1 * value2
+        elif self.operation == "/":
+          self.answer = value1 / value2
+        elif self.operation == "^":
+          self.answer = value1 ** value2
+        elif self.operation == "√":
+          self.answer = value2 ** (1/value1)
       else:
-        value1 = float(self.value1)
-      if "π" in self.value2:
-        value2 = float(self.value2[:-1]) * math.pi
-      else:
-        value2 = float(self.value2)
-      if self.operation == "+":
-        self.answer = value1 + value2
-      elif self.operation == "-":
-        self.answer = value1 - value2
-      elif self.operation == "X":
-        self.answer = value1 * value2
-      elif self.operation == "/":
-        self.answer = value1 / value2
+        self.answer = self.value1
+      if float(self.answer) == int(self.answer):
+        self.answer = int(self.answer)
       self.answer = str(self.answer)
-      self.cor_sign()
       self.value1 = self.answer
       self.value2 = ""
       self.operation = ""
@@ -247,54 +316,4 @@ class Calculator(discord.ui.View):
     embed = self.emb()
     await interaction.response.edit_message(embed=embed)
 
-  @discord.ui.button(label = "Sin", row = 0, style = discord.ButtonStyle.blurple)
-  async def sin(self, interaction: discord.Interaction, button: discord.ui.Button):
-    if self.operation == "" and self.value2 == "":
-      self.answer = str(round(math.sin(self.pi_con(self.value1)), 10))
-      self.cor_sign()
-      self.selected = self.answer
-      self.value1 = "0"
-      self.value2 = ""
-      self.operation = ""
-      self.change_val()
-    else:
-      self.call_error()
-    embed = self.emb()
-    await interaction.response.edit_message(embed=embed)
-
-  @discord.ui.button(label = "Cos", row = 1, style = discord.ButtonStyle.blurple)
-  async def cos(self, interaction: discord.Interaction, button: discord.ui.Button):
-    if self.operation == "" and self.value2 == "":
-      self.answer = str(round(math.cos(self.pi_con(self.value1)), 10))
-      self.cor_sign()
-      self.selected = self.answer
-      self.value1 = "0"
-      self.value2 = ""
-      self.operation = ""
-      self.change_val()
-    else:
-      self.call_error()
-    embed = self.emb()
-    await interaction.response.edit_message(embed=embed)
-
-  @discord.ui.button(label = "Tan", row = 2, style = discord.ButtonStyle.blurple)
-  async def tan(self, interaction: discord.Interaction, button: discord.ui.Button):
-    if self.operation == "" and self.value2 == "":
-      self.answer = str(round(math.tan(self.pi_con(self.value1)), 10))
-      self.cor_sign()
-      self.selected = self.answer
-      self.value1 = "0"
-      self.value2 = ""
-      self.operation = ""
-      self.change_val()
-    else:
-      self.call_error()
-    embed = self.emb()
-    await interaction.response.edit_message(embed=embed)
-
-  @discord.ui.button(label = "π", row = 3, style = discord.ButtonStyle.blurple)
-  async def pi(self, interaction: discord.Interaction, button: discord.ui.Button):
-    self.selected = "π"
-    self.change_val()
-    embed = self.emb()
-    await interaction.response.edit_message(embed=embed)
+  
