3763821581260df032590367777f4c34db517c4c
michaelkalai <113479090+michaelkalai@users.noreply.github.com>
2024-02-24 21:46:39 -0500

    Delete barber_finder.py

--- barber_finder.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-
-class Barber_Finder():
-    def echo():
-      print("man u ugly")
-      url = "https://www.youtube.com/@CoryxKenshin"
-      driver = webdriver.Chrome()
-      driver.get(url)
